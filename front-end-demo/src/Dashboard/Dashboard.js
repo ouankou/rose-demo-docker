67856f77cb6a9ee3cee425315f00ccc889586c6e
Kewei Yan <yankw62@gmail.com>
2020-05-03 00:24:05 -0400

    flex fixed, display rotation added

--- front-end-demo/src/Dashboard/Dashboard.js
+++ front-end-demo/src/Dashboard/Dashboard.js
@@ -18,10 +18,22 @@ children.push(<Option key="7">LIB:MPI</Option>);
 children.push(<Option key="8">LIB:MKL</Option>);
 
 class Dashboard extends React.Component {
-    state = {};
+    state = {
+        myDisplay: 'row'
+    };
     
     componentDidMount() {
         
+    };
+              
+    Onclk = () => {
+       if (this.state.myDisplay == 'row') {
+           this.setState({myDisplay: 'column'});
+       } else {
+           this.setState({myDisplay: 'row'});
+       }
+
+       console.log('display changed!');
     };
               
     render() {
@@ -37,19 +49,24 @@ class Dashboard extends React.Component {
                     <Button>Home</Button>
                     <Button>Upload</Button>
                     <Button>Save</Button>
+                    <Button
+                        onClick = {() => this.Onclk()}
+                    >Rotate</Button>
                     <Button type="primary">Compile</Button>
+                </div>
+                <div className = "tags">
                     <Select
                       mode="multiple"
-                      style={{ width: '77%' }}
+                      style={{ width: '100%' }}
                       placeholder="Please select compile configurations"
                       defaultValue={['COMPILER:GCC8.0', 'RUNTIME:LLVM']}
                     >
                       {children}
                     </Select>
                 </div>
-                <div className = "texts">
-                    <textarea className = "textbox" placeholder="Type or upload your code"/>
-                    <textarea className = "textbox" placeholder="Results/Error messages will be shown here"/>
+                <div className = "texts" style = {{flexDirection: this.state.myDisplay}}>
+                    <textarea className = "inputbox" placeholder="Type or upload your code"/>
+                    <textarea className = "outputbox" placeholder="Results/Error messages will be shown here"/>
                 </div>
             </div>
         )
